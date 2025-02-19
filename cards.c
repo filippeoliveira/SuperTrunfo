642f84553fe5280e2206535e533692bd0bc79dc4
Filippe F Oliveira <filippeoliveira@gmail.com>
2025-02-19 16:26:35 -0300

    update files

--- cards.c
+++ cards.c
@@ -90,40 +90,212 @@ void cards {
             cards9.pontosTuristicos = 150;
 
     struct cards cards10;
-
-
-
-
-
+            strcpy(cards10.estado, "C");
+            cards10.codigoCarta, "C02";
+            cards10.nomeCidade, "Niterói";
+            cards10.populacao = 500000;
+            cards10.area = 129.3;
+            cards10.pib = 10000000.0;
+            cards10.pontosTuristicos = 50;
 
     struct cards cards11;
-
-
-
-
-
+            strcpy(cards11.estado, "C");
+            cards11.codigoCarta, "C03";
+            cards11.nomeCidade, "Petrópolis";
+            cards11.populacao = 300000;
+            cards11.area = 795;
+            cards11.pib = 5000000.0;
+            cards11.pontosTuristicos = 50;
 
     struct cards cards12;
+            strcpy(cards12.estado, "C");
+            cards12.codigoCarta, "C04";
+            cards12.nomeCidade, "AngraDosReis";
+            cards12.populacao = 200000;
+            cards12.area = 825;
+            cards12.pib = 1000000.0;
+            cards12.pontosTuristicos = 50;
+
     struct cards cards13;
+            strcpy(cards13.estado, "D");
+            cards13.codigoCarta, "D01";
+            cards13.nomeCidade, "SãoPaulo";
+            cards13.populacao = 12000000;
+            cards13.area = 1521;
+            cards13.pib = 700000000.0;
+            cards13.pontosTuristicos = 200;
+
     struct cards cards14;
+            strcpy(cards14.estado, "D");
+            cards14.codigoCarta, "D02";
+            cards14.nomeCidade, "Campinas";
+            cards14.populacao = 1200000;
+            cards14.area = 795;
+            cards14.pib = 50000000.0;
+            cards14.pontosTuristicos = 50;
+
     struct cards cards15;
+            strcpy(cards15.estado, "D");
+            cards15.codigoCarta, "D03";
+            cards15.nomeCidade, "Santos";
+            cards15.populacao = 500000;
+            cards15.area = 280;
+            cards15.pib = 20000000.0;
+            cards15.pontosTuristicos = 50;
+
     struct cards cards16;
+            strcpy(cards16.estado, "D");
+            cards16.codigoCarta, "D04";
+            cards16.nomeCidade, "SãoJoséDosCampos";
+            cards16.populacao = 700000;
+            cards16.area = 1096;
+            cards16.pib = 30000000.0;
+            cards16.pontosTuristicos = 50;
+            
     struct cards cards17;
+            strcpy(cards17.estado, "E");
+            cards17.codigoCarta, "E01";
+            cards17.nomeCidade, "Curitiba";
+            cards17.populacao = 2000000;
+            cards17.area = 430;
+            cards17.pib = 100000000.0;
+            cards17.pontosTuristicos = 50;
+            
     struct cards cards18;
+            strcpy(cards18.estado, "E");
+            cards18.codigoCarta, "E02";
+            cards18.nomeCidade, "FozDoIguaçu";
+            cards18.populacao = 300000;
+            cards18.area = 617;
+            cards18.pib = 20000000.0;
+            cards18.pontosTuristicos = 50;
+
     struct cards cards19;
+            strcpy(cards19.estado, "E");
+            cards19.codigoCarta, "E03";
+            cards19.nomeCidade, "PontaGrossa";
+            cards19.populacao = 200000;
+            cards19.area = 211;
+            cards19.pib = 10000000.0;
+            cards19.pontosTuristicos = 50;
+
     struct cards cards20;
+            strcpy(cards20.estado, "E");
+            cards20.codigoCarta, "E04";
+            cards20.nomeCidade, "Londrina";
+            cards20.populacao = 500000;
+            cards20.area = 165;
+            cards20.pib = 30000000.0;
+            cards20.pontosTuristicos = 50;
+
     struct cards cards21;
+            strcpy(cards21.estado, "F");
+            cards21.codigoCarta, "F01";
+            cards21.nomeCidade, "PortoAlegre";
+            cards21.populacao = 1500000;
+            cards21.area = 496;
+            cards21.pib = 80000000.0;
+            cards21.pontosTuristicos = 50;
+
     struct cards cards22;
+            strcpy(cards22.estado, "F");
+            cards22.codigoCarta, "F02";
+            cards22.nomeCidade, "Gramado";
+            cards22.populacao = 50000;
+            cards22.area = 237.6;
+            cards22.pib = 1000000.0;
+            cards22.pontosTuristicos = 50;
+
     struct cards cards23;
+            strcpy(cards23.estado, "F");
+            cards23.codigoCarta, "F03";
+            cards23.nomeCidade, "CaxiasDoSul";
+            cards23.populacao = 500000;
+            cards23.area = 1641;
+            cards23.pib = 30000000.0;
+            cards23.pontosTuristicos = 50;
+
     struct cards cards24;
+            strcpy(cards24.estado, "F");
+            cards24.codigoCarta, "F04";
+            cards24.nomeCidade, "SantaMaria";
+            cards24.populacao = 300000;
+            cards24.area = 1500;
+            cards24.pib = 20000000.0;
+            cards24.pontosTuristicos = 50;
+
     struct cards cards25;
+            strcpy(cards25.estado, "G");
+            cards25.codigoCarta, "G01";
+            cards25.nomeCidade, "Brasília";
+            cards25.populacao = 3000000;
+            cards25.area = 5801;
+            cards25.pib = 200000000.0;
+            cards25.pontosTuristicos = 50;
+
     struct cards cards26;
+            strcpy(cards26.estado, "G");
+            cards26.codigoCarta, "G02";
+            cards26.nomeCidade, "Goiânia";
+            cards26.populacao = 1500000;
+            cards26.area = 739;
+            cards26.pib = 100000000.0;
+            cards26.pontosTuristicos = 50;
+
     struct cards cards27;
+            strcpy(cards27.estado, "G");
+            cards27.codigoCarta, "G03";
+            cards27.nomeCidade, "Cuiabá";
+            cards27.populacao = 600000;
+            cards27.area = 356;
+            cards27.pib = 30000000.0;
+            cards27.pontosTuristicos = 50;
+
     struct cards cards28;
+            strcpy(cards28.estado, "G");
+            cards28.codigoCarta, "G04";
+            cards28.nomeCidade, "Palmas";
+            cards28.populacao = 300000;
+            cards28.area = 221;
+            cards28.pib = 20000000.0;
+            cards28.pontosTuristicos = 50;
+
     struct cards cards29;
+            strcpy(cards29.estado, "H");
+            cards29.codigoCarta, "H01";
+            cards29.nomeCidade, "Salvador";
+            cards29.populacao = 3000000;
+            cards29.area = 706;
+            cards29.pib = 200000000.0;
+            cards29.pontosTuristicos = 50;
+
     struct cards cards30;
+            strcpy(cards30.estado, "H");
+            cards30.codigoCarta, "H02";
+            cards30.nomeCidade, "PortoSeguro";
+            cards30.populacao = 150000;
+            cards30.area = 2595;
+            cards30.pib = 10000000.0;
+            cards30.pontosTuristicos = 50;
+
     struct cards cards31;
+            strcpy(cards31.estado, "H");
+            cards31.codigoCarta, "H03";
+            cards31.nomeCidade, "Ilhéus";
+            cards31.populacao = 200000;
+            cards31.area = 192;
+            cards31.pib = 30000000.0;
+            cards31.pontosTuristicos = 50;
+
     struct cards cards32;
+            strcpy(cards32.estado, "H");
+            cards32.codigoCarta, "H04";
+            cards32.nomeCidade, "Itabuna";
+            cards32.populacao = 200000;
+            cards32.area = 432;
+            cards32.pib = 20000000.0;
+            cards32.pontosTuristicos = 50;
+            
 
     
     
